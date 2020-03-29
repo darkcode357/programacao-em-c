ab0a1edc90347cc2eeb3b22d36a50fc618cf4858
darkcode357 <darkcode357@gmail.com>
2020-03-29 17:56:24 -0300

    aula7 concluida

--- source/aula7.c
+++ source/aula7.c
@@ -2,7 +2,16 @@
 
 int main()
 {
-    int a = 12;
-    int b = 2;
-    printf("%d%d", a, b);
+    int base;
+    int altura;
+    int area;
+
+    printf("Digite o valor da base:");
+    scanf("%d", &base);
+
+    printf("Digite o valor da altura:");
+    scanf("%d", &altura);
+
+    area = base * altura;
+    printf("o valor da area do retangulo e :[%d]", area);
 }
